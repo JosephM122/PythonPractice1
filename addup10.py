bef0135b33788a4eca809c1005408277868a0759
JosephM122 <105502006+JosephM122@users.noreply.github.com>
2022-05-19 20:07:56 -0500

    Update addup10.py

--- addup10.py
+++ addup10.py
@@ -1,8 +1,3 @@
-#have this function return True or False, do two given numbers add up to 10?
-#complete the function as your answer
-def addup10(a, b):
-    return
-
-x = input("First num: ")
-y = input("Second num: ")
-print(addup10(x, y))
+x = 6
+y = 4
+print(x + y == 10)
