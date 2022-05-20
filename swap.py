d90a236c5102f36df24fba20ee3f819871765979
JosephM122 <105502006+JosephM122@users.noreply.github.com>
2022-05-20 00:35:30 -0500

    Update swap.py

--- swap.py
+++ swap.py
@@ -1,8 +1,12 @@
-#given a list of integers, swap the first and last elements, return the new list with swapped elements
-#complete the function as your answer
-def swap(numList):
-    return
-
-print(swap([1,2,3,4,5]))        #should print [5,2,3,4,1]
-print(swap([5,2,3,4,1]))        #should print [1,2,3,4,5]
-print(swap([7,4,7,2,3,6,3]))    #should print [3,4,7,2,3,6,7]
+list1 = [1,2,3,4,5]        #should print [5,2,3,4,1]
+list2 = [5,2,3,4,1]        #should print [1,2,3,4,5]
+list3 = [7,4,7,2,3,6,3]    #should print [3,4,7,2,3,6,7]
+list1[0] = 5
+list1[4] = 1
+print(list1)
+list2[0] = 1
+list2[4] = 5
+print(list2)
+list3[0] = 3
+list3[6] = 7
+print(list3)
