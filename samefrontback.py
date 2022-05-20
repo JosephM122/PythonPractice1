340e1200d7e0134efc88874d331cf49e041fef6a
JosephM122 <105502006+JosephM122@users.noreply.github.com>
2022-05-19 23:49:30 -0500

    Update samefrontback.py

--- samefrontback.py
+++ samefrontback.py
@@ -1,12 +1,30 @@
-#given a list of integers, return True if the length of the list is greater than 1 AND
-#the first element and the last element are equal
-#complete the function as your answer
-def sameFrontBack(numbers):
-    return
-
-print(sameFrontBack([1,2,3]))              #should return False
-print(sameFrontBack([1,2,3,1]))            #should return True
-print(sameFrontBack([1,2,1]))              #should return True
-print(sameFrontBack([1,2,3,4,5,6,7]))      #should return False
-print(sameFrontBack([]))                   #should return False
-print(sameFrontBack([1]))                   #should return False
+list1 = [1,2,3]
+list2 = [1,2,3,1]
+list3 = [1,2,1]
+list4 = [1,2,3,4,5,6,7]
+list5 = []
+list6 = [1]
+if len(list1) > 1 and list1[0] == list1[-1]:
+    print(True)
+else:
+    print(False)
+if len(list2) > 1 and list2[0] == list2[-1]:
+    print(True)
+else:
+    print(False)
+if len(list3) > 1 and list3[0] == list3[-1]:
+    print(True)
+else:
+    print(False)
+if len(list4) > 1 and list4[0] == list4[-1]:
+    print(True)
+else:
+    print(False)
+if len(list5) > 1 and list5[0] == list5[-1]:
+    print(True)
+else:
+    print(False)
+if len(list6) > 1 and list6[0] == list6[-1]:
+    print(True)
+else:
+    print(False)
